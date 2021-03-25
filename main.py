4fbf19623623a0d75d0cd6727a9a83559f2e3d6e
Houfin64 <67628989+Houfin64@users.noreply.github.com>
2021-03-25 10:47:57 +0000

    made all prev functions work
    and added the baseplates of the ordering functions

--- main.py
+++ main.py
@@ -28,32 +28,27 @@ async def ping(ctx):
     await ctx.send(embed=embed)
 
 @bot.command(name="create-shop")
-async def create_shop(ctx):
+async def create_shop(ctx, *, argument):
 
     sdict = await load_json("shops")
     udict = await load_json("users")
 
-    embed = discord.Embed(title="Welcome to the Shop interface!", description="Please tell me the name of your shop!", color=random.randint(0, 16777215))
-    await ctx.send(embed=embed)
+    if "|" not in argument:
+        embed = discord.Embed(title="Welp, that failed!", description="Syntax: `£create-shop <shop-name> | <description>`", color=0xff0000)
+        return await ctx.send(embed=embed)
 
-    message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
-    content = message.content
-    name = content
+    name, description = argument.partition("|")[0], argument.partition("|")[2]
+
     owner = ctx.author.id
+    
     if str(owner) in udict:
-        embed = discord.Embed(title="Welp that failed!", description="You can't have more than 1 shop, add items instead.  \nIf you want to delete your shop do `£remove-shop` instead, and i will take yor stall down.", color=random.randint(0, 16777216))
+        embed = discord.Embed(title="Welp that failed!", description="You can't have more than 1 shop, add items instead.  \nIf you want to delete your shop do `£remove-shop` instead, and i will take yor stall down.", color=0xff0000)
         return await ctx.send(embed=embed)
     if name in sdict:
-        embed = discord.Embed(title="Welp that failed", description="To avoid errors, please have a unique shop name", color=random.randint(0, 16777215))
+        embed = discord.Embed(title="Welp that failed", description="To avoid errors, please have a unique shop name", color=0xff0000)
         return await ctx.send(embed=embed)
 
-    embed = discord.Embed(title="Welcome to the Shop interface!", description="Please give me a description of your shop!", color=random.randint(0, 16777215))
-    await ctx.send(embed=embed)
-
-    message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
-    content = message.content
-    description = content
-    embed = discord.Embed(title="KK, Thanks", description="I've added you to the shop list :)! do `£add-item` to add items to your shop.", color=random.randint(0, 16777215))
+    embed = discord.Embed(title="KK, Thanks", description="I've added you to the shop list :)! do `£add-item` to add items to your shop.", color=0xff0000)
     await ctx.send(embed=embed)
 
     udict[str(ctx.author.id)] = name
@@ -68,56 +63,51 @@ async def remove_shop(ctx):
     udict = await load_json("users")
 
     if str(ctx.author.id) not in udict:
-        embed = discord.Embed(title="You don't have a shop dumbass!", description="Do `£create-shop` to make one :)", color=random.randint(0, 16777215))
+        embed = discord.Embed(title="You don't have a shop dumbass!", description="Do `£create-shop` to make one :)", color=0xff0000)
         return await ctx.send(embed=embed)
 
     del sdict[udict[str(ctx.author.id)]]
     del udict[str(ctx.author.id)]
     
-    embed = discord.Embed(title="Your stall has been taken down!", color=random.randint(0, 16777215))
+    embed = discord.Embed(title="Your stall has been taken down!", color=0x00ff00)
     await ctx.send(embed=embed)
 
     await json_write("users", udict)
     await json_write("shops", sdict)
 
 @bot.command(name="add-item")
-async def add_item(ctx):
+async def add_item(ctx, *, argument):
+
+    if "|" not in argument:
+        embed = discord.Embed(title="Welp, that failed!", description="Syntax: `£add-item <item-name> | <price>`", color=0xff0000)
+        return await ctx.send(embed=embed)
+
+    item, price = argument.partition("|")[0], argument.partition("|")[2]
+
     sdict = await load_json("shops")
     udict = await load_json("users")
 
-    embed = discord.Embed(title="Welcome to the Shop interface!", description="Please tell me the name of the item you are selling!", color=random.randint(0, 16777215))
-    await ctx.send(embed=embed)
-
-    message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
-    item = message.content
-
-    embed = discord.Embed(title="Welcome to the Shop interface!", description="How much are you selling them for?", color=random.randint(0, 16777215))
-    await ctx.send(embed=embed)
-
-    message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
-    price = message.content
-
+    try:
+        if sdict[udict[str(ctx.author.id)]]["items"][item]:
+            embed = discord.Embed(title="Welp, that failed!", description="You already have that item in your shop.  \nDo `£remove-item` if you want to change the price!", color=0xff0000)
+            return await ctx.send(embed=embed)
+    except KeyError:
+        sdict[udict[str(ctx.author.id)]]["items"][item] = price
     sdict[udict[str(ctx.author.id)]]["items"][item] = price
-    
-    embed = discord.Embed(title="That's you done :)", description="Your item has been added to your shop", color=random.randint(0, 16777215))
+
+    embed = discord.Embed(title="That's you done :)", description="Your item has been added to your shop", color=0x00ff00)
     await ctx.send(embed=embed)
 
     await json_write("shops", sdict)
 
 @bot.command(name="remove-item")
-async def remove_item(ctx):
+async def remove_item(ctx, item):
     sdict = await load_json("shops")
     udict = await load_json("users")
 
-    embed = discord.Embed(title="Welcome to the Shop interface!", description="Please tell me the name of the item you are removing!", color=random.randint(0, 16777215))
-    await ctx.send(embed=embed)
-
-    message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
-    item = message.content
-
     del sdict[udict[str(ctx.author.id)]]["items"][item]
     
-    embed = discord.Embed(title="That's you done :)", description="Your item has been removed from your shop", color=random.randint(0, 16777215))
+    embed = discord.Embed(title="That's you done :)", description="Your item has been removed from your shop", color=0x00ff00)
     await ctx.send(embed=embed)
 
     await json_write("shops", sdict)
@@ -141,7 +131,7 @@ async def shop(ctx, *, profiles: Member_Obj = None):
     if profiles == None:
         profiles = [ctx.author]
     if len(profiles) > 1:
-        embed = discord.Embed(description="Be more specific for goodness's sake!", color=random.randint(0, 16777215))
+        embed = discord.Embed(description="Be more specific for goodness's sake!", color=0xff0000)
         return await ctx.send(embed=embed)
     user = profiles[0]
 
@@ -155,5 +145,22 @@ async def shop(ctx, *, profiles: Member_Obj = None):
         embed.add_field(name=elem, value=sdict[udict[str(user.id)]]["items"][elem])
     embed.set_footer(text=f"requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
+
+@bot.command(name="place-order")
+async def place_order(ctx, shop, item, quantity):
+    pass
+    # NOTE:  DM will be sent automatically when order is placed, and a second DM will be sent when revoked to apologise for useless DM
+
+@bot.command(name="revoke-order")
+async def revoke_order(ctx, order):
+    pass
+
+@bot.command(name="outgoing-orders")
+async def my_orders(ctx):
+    pass
+
+@bot.command(name="incoming-orders")
+async def shop_orders(ctx):
+    pass
 
 bot.run('ODIzOTI4NzcwMTIwNTE1NjY1.YFn9dg.lCoqMxOwpVnehLnLEjdX1Hsi0rs')
