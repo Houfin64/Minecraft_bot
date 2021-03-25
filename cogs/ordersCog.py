26f725390ab45486bb567e5c82e2b279851a3732
Houfin64 <67628989+Houfin64@users.noreply.github.com>
2021-03-25 15:20:45 +0000

    made revoking orders

--- cogs/ordersCog.py
+++ cogs/ordersCog.py
@@ -28,6 +28,9 @@ class Orders(commands.Cog):
         if item not in sdict[shop]["items"]:
             embed = discord.Embed(title="Welp that failed!", description="That item doesn't exist. use `£shop <user>` to see the existing ones", color=0xff0000)
             return await ctx.send(embed=embed)
+        if item in oodict[orderer]:
+            embed = discord.Embed(title="Welp that failed!", description="You already have an outstanding order of that, `£revoke-order` to make a new one", color=0xff0000)
+            return await ctx.send(embed=embed)
         try:
             tprice = int(quantity) * int(sdict[shop]["items"][item])
         except ValueError:
@@ -36,13 +39,18 @@ class Orders(commands.Cog):
         
         orderee = sdict[shop]["owner"]
 
-        iodict[orderee] = {"orderer": orderer, "tprice": tprice, "quantity": quantity, "shop": shop, "item": item}
-        oodict[orderer] = {"orderee": orderee, "tprice": tprice, "quantity": quantity, "shop": shop, "item": item}
+        if orderee not in iodict:
+            iodict[orderee] = {"item": {"orderer": orderer, "tprice": tprice, "quantity": quantity, "shop": shop, "item": item}}
+        if orderer not in oodict:
+            oodict[orderer] = {"item": {"orderee": orderee, "tprice": tprice, "quantity": quantity, "shop": shop, "item": item}}
 
         await self.bot.get_user(int(orderee)).create_dm()
 
-        embed = discord.Embed(title="An Order has been placed with your shop", description="For {} {}s for a total price of {}".format(quantity, item, tprice), colour=0x00ff00)
+        embed = discord.Embed(title="An Order has been placed with your shop", description="For {} {}s for a total price of {}".format(quantity, item, tprice), color=0x00ff00)
         await self.bot.get_user(int(orderee)).dm_channel.send(embed=embed)
+
+        embed = discord.Embed(title="Succes!", description="Your order has been placed", color=0x00ff00)
+        await ctx.send(embed=embed)
 
         await json_write("orders", iodict)
         await json_write("out_orders", oodict)
@@ -51,7 +59,29 @@ class Orders(commands.Cog):
 
     @commands.command(name="revoke-order")
     async def revoke_order(self, ctx, order):
-        pass
+        udict = await load_json("users")
+        sdict = await load_json("shops")
+        iodict = await load_json("orders")
+        oodict = await load_json("out_orders")
+
+        if str(ctx.author.id) not in oodict:
+            embed = discord.Embed(title="Welp that failed!", description="You don't have any outgoing orders", color=0xff0000)
+            return await ctx.send(embed=embed)
+        if order not in oodict[str(ctx.author.id)]:
+            embed = discord.Embed(title="Welp that failed!", description="You don't have an outstanding order of that item", color=0xff0000)
+            return await ctx.send(embed=embed)
+
+        orderee = oodict[ctx.author.id]["orderee"]
+        del oodict[ctx.author.id][order]
+        del iodict[int(orderee)][order]
+
+        embed = discord.Embed(title="Succes!", description="Your order has been revoked", color=0x00ff00)
+        await ctx.send(embed=embed)
+
+        await self.bot.get_user(int(orderee)).create_dm()
+
+        embed = discord.Embed(title="An order has been revoked", description="Sorry for previous ping, but the order made has now been revoked by the orderer".format(quantity, item, tprice), color=0xff0000)
+        await self.bot.get_user(int(orderee)).dm_channel.send(embed=embed)
 
     @commands.command(name="outgoing-orders")
     async def my_orders(self, ctx):
